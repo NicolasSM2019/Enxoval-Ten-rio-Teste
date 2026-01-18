e0f92a82c70b2e766e9477f376da515c578a9e8f
vitortenorio330-creator <vitortenorio330@gmail.com>
2026-01-18 00:48:17 -0300

    mudei o nome do casal

--- src/Entrada.jsx
+++ src/Entrada.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
     <div style={{color: 'hsl(204, 87%, 21%)', fontSize: '35px' , padding: '20px'}}>Bem-Vindo ao Site Do Casal</div>
- <Link to="/acessar "  className='titulo'>Enxoval Tenorio e Silva</Link>
+ <Link to="/acessar "  className='titulo'>Enxoval Carleane e João Vitor</Link>
  
  
   </div>
